ec7f3ac37c99f36083db9ddc8fbc66576b92c19d
GYAN DASH <gyanaranjan.dash5@gmail.com>
2020-12-31 15:07:05 +0530

    Add project files.

---
+++ PaymentAPI/Models/PaymentDetail.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PaymentAPI.Models
+{
+    public class PaymentDetail
+    {
+        [Key]
+       
+        public int PaymentDetailId { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        [Column(TypeName ="nvarchar(100)")]
+        public string CardOwnerName { get; set; }
+
+        [Required]
+        [StringLength(16)]
+        [Column(TypeName = "nvarchar(16)")]
+        public string CardNumber { get; set; }
+
+        [Required]
+        [StringLength(5)]
+        [Column(TypeName = "nvarchar(5)")] //dd/yy
+        public string ExpirationDate { get; set; }
+
+        [Required]
+        [StringLength(3)]
+        [Column(TypeName = "nvarchar(3)")]
+        public string SecurityCode { get; set; }
+    }
+}
